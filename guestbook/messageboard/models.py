e24de8e21af2891aa8db12f5887192fce6b2f147
rabboh <pal.balazs2@gmail.com>
2022-12-07 18:14:31 +0100

    fejl

--- guestbook/messageboard/models.py
+++ guestbook/messageboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Messageboard(models.Model):
+    nickname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    message = models.CharField(max_length=255)
+    postdate = models.DateField(auto_now=True)
+    
--- guestbook/messageboard/views.py
+++ guestbook/messageboard/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Messageboard
+
 
 def index(request):
+    if (request.method == 'POST'):
+        nickname = request.POST['nickname']
+        email = request.POST['email']
+        message = request.POST['message']
+        newEntry = Messageboard(nickname=nickname, email=email, message=message)
+        newEntry.save()
+
+    entries = Messageboard.objects.all().values()
+    context = {
+        'entries': entries
+    }
+
     template = loader.get_template('frontend.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context, request))
 
